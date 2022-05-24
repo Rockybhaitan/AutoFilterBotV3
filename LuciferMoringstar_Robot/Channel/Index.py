ee332d004747821751920ebc4bcfb75bc0ddd5e5
Rocky Bhai <106011893+Rockybhaitan@users.noreply.github.com>
2022-05-24 19:27:15 +0530

    Update Index.py

--- LuciferMoringstar_Robot/Channel/Index.py
+++ LuciferMoringstar_Robot/Channel/Index.py
@@ -79,11 +79,11 @@ async def index_files(bot, message):
             else:
                 await msg.edit(f'Total {total_files} Saved To DataBase!')
 
-RATING = ["5.1 | IMDB", "6.2 | IMDB", "7.3 | IMDB", "8.4 | IMDB", "9.5 | IMDB", ]
-GENRES = ["fun, fact",
-         "Thriller, Comedy",
-         "Drama, Comedy",
-         "Family, Drama",
-         "Action, Adventure",
-         "Film Noir",
-         "Documentary"]
+RATING = ["700MB-800MB",700MB-800MB"]
+GENRES = ["HD",
+         "HD",
+         "HD",
+         "HD",
+         "HD",
+         "HD",
+         "HD"]
